f9995598d64837bf4d68f26cdd9dd6888fa07030
divyeshcodez <divyeshcodez@users.noreply.github.com>
2026-08-03 23:22:42 +0530

    chore: remove sections below lookbook slider

--- src/pages/Home/HomePage.tsx
+++ src/pages/Home/HomePage.tsx
@@ -1,22 +1,14 @@
 import React from 'react';
 import { LookbookSlider } from '../../components/LookbookSlider/LookbookSlider';
-import { FeaturedDrop } from './FeaturedDrop';
-import { BrandStatement } from './BrandStatement';
-import { ShopByCategory } from './ShopByCategory';
-import { FinalCTA } from './FinalCTA';
 
 interface HomePageProps {
   onNavigateToShop: () => void;
 }
 
-export const HomePage: React.FC<HomePageProps> = ({ onNavigateToShop }) => {
+export const HomePage: React.FC<HomePageProps> = () => {
   return (
     <div className="bg-[#0B0B0C] min-h-screen text-white w-full flex flex-col">
       <LookbookSlider />
-      <FeaturedDrop onViewPiece={onNavigateToShop} />
-      <BrandStatement />
-      <ShopByCategory />
-      <FinalCTA onExploreClick={onNavigateToShop} />
     </div>
   );
 };
